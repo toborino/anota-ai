d05b69ed201c137eef7d119255b4d30ebfc97ad4
= <email4.m.rahman@gmail.com>
2016-06-24 04:31:08 +0200

    token now in config

--- app/background.js
+++ app/background.js
@@ -3,6 +3,5 @@ var bot = require('./bot.js')
 var interval = setInterval(function() {
 	console.log('checking for reminders from background process #' + process.pid);
 	var _bot = new bot()
-	_bot.setToken(token);
 	_bot.sendReminders();
 }, 30000);
