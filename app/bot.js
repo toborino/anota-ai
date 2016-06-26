28c53b9e1bee5350bb8c2a8439d4ecc18278b821
= <email4.m.rahman@gmail.com>
2016-06-26 11:13:11 +0200

    error

--- app/bot.js
+++ app/bot.js
@@ -224,6 +224,7 @@ bot.prototype =
 									{
 										if(err)
 										{
+											that.res.send('Error occured, but we have been notified!')
 											return console.log(err)
 										}
 										that.res.send(data)									
