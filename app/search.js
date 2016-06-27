1ee12959c78ad5c32f854b17e7f40c2f04f00f70
= <email4.m.rahman@gmail.com>
2016-06-27 10:39:54 +0200

    note id

--- app/search.js
+++ app/search.js
@@ -64,7 +64,7 @@ search.prototype = {
 			}
 		}
 		
-		this.bot.pgClient.query('SELECT notes.*, topics.topic AS topic FROM notes LEFT JOIN topics ON topics.note_id = notes.id WHERE id = $3 AND user_id = $1 AND notified = FALSE and reminder_at >= $2 ORDER BY reminder_at ASC', [this.event.sender.id, dateformat(new Date(), 'yyyy-mm-dd H:MM:00'),  note_id], 
+		this.bot.pgClient.query('SELECT notes.*, topics.topic AS topic FROM notes LEFT JOIN topics ON topics.note_id = notes.id WHERE notes.id = $3 AND user_id = $1 AND notified = FALSE and reminder_at >= $2 ORDER BY reminder_at ASC', [this.event.sender.id, dateformat(new Date(), 'yyyy-mm-dd H:MM:00'),  note_id], 
 			function(err, result)
 			{
 				if(err)
