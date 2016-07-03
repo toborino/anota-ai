2c9b59ddff68d2e2764fc49d67b240895b55f878
= <email4.m.rahman@gmail.com>
2016-07-03 08:39:53 +0200

    you too see

--- app/bot.js
+++ app/bot.js
@@ -233,7 +233,7 @@ bot.prototype =
 							}
 							else
 							{
-								that.sendTextMessage(row.user_id, 'Got it! You are ' + require('./timeFormat.js').offsetToTimezone(timezone));
+								that.sendTextMessage(row.user_id, 'Got it! You are UTC' + require('./timeFormat.js').offsetToTimezone(timezone));
 								require('fs').readFile(__dirname + '/template/timezone-set.html', 'utf8', function (err, data)
 									{
 										if(err)
