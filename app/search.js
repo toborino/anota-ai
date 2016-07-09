536fc9541d9f8e350d51f9383a1f6ff0110935dc
= <email4.m.rahman@gmail.com>
2016-07-09 13:23:38 +0200

    rows log

--- app/search.js
+++ app/search.js
@@ -18,7 +18,7 @@ search.prototype = {
 	
 	prepareNotesForDisplay: function(result)
 	{
-		console.log(result);
+		console.log(result.rows);
 		var that = this
 		var elements = [];
 		for(var i = 0; i < result.rows.length; i++)
