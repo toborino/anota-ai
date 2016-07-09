b6ab231b47471ac3374d67798472787ef9f9c9d2
= <email4.m.rahman@gmail.com>
2016-07-09 10:52:16 +0200

    log whats causing syntax error

--- app/bot.js
+++ app/bot.js
@@ -17,7 +17,7 @@ bot.prototype =
 		messaging_events = this.req.body.entry[0].messaging
 		for (i = 0; i < messaging_events.length; i++) {
 			event = this.req.body.entry[0].messaging[i]
-
+console.log(event.postback, event.postback.payload);
 			sender = event.sender.id
 			if (event.postback) {
 				console.log(event.postback.payload)
